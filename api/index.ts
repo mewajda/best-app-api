690bd7515fec2ad9de4d75bae17745d98f05b814
Mike Wajda <mewajda@gmail.com>
2025-07-24 12:20:15 -0500

    feat: cors

--- api/index.ts
+++ api/index.ts
@@ -1,7 +1,10 @@
 import { handle } from 'hono/vercel';
 import { Hono } from 'hono';
+import { cors } from 'hono/cors';
 
 const app = new Hono().basePath('/api');
+
+app.use('*', cors());
 
 app.get('/health', (c) => {
     return c.json({
